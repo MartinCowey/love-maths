8e266b15d7cb50234a2a1014db6e893bf0ea25a0
Martin Cowey <starman.martin@gmail.com>
2024-10-15 10:38:54 +0000

    Add addition function and change code in runGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,11 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
-
+runGame("addition");
 
 });
 
@@ -26,10 +26,17 @@ document.addEventListener("DOMContentLoaded", function() {
  */
 
 
-function runGame() {
+function runGame(gameType) {
     //creates 2 random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if(gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
 }
 
@@ -50,8 +57,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
